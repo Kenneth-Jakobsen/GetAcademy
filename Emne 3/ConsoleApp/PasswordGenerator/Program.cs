f330a8008b00e41abd0f94cc37767949ff52a657
ImPhame <jakobsenkenneth@hotmail.no>
2024-11-25 13:40:56 +0100

    update

--- Emne 3/ConsoleApp/PasswordGenerator/Program.cs
+++ Emne 3/ConsoleApp/PasswordGenerator/Program.cs
@@ -3,18 +3,19 @@
     class Program
     {
        private static void Main(string[] args)
-        {
+       {
+           string[]? arguments = Console.ReadLine()?.Split(' ');
             var smallLetter = 0;
             var capitalLetter = 0;
             var specialChars = 0;
             var number = 0;
-            if (!IsValid(args))
+            if (!IsValid(arguments))
             {
                 PasswordGenerator();
             }
             else
             {
-                foreach (var c in args[1])
+                foreach (var c in arguments?[1]!)
                 {
                     switch (c)
                     {
@@ -37,47 +38,47 @@
                               $"{capitalLetter} capital letters, " +
                               $"{number} numbers " +
                               $"and {specialChars} special characters " +
-                              $"and be {args[0]} characters long");
+                              $"and be {arguments[0]} characters long");
             }
-        }
+       }
 
-        private static bool IsValid(string[] args)
-        {
-            var number = int.Parse(args[0]);
-            var requirements = args[1];
-            if (number < 6 || requirements==" ") return false;
-            foreach (var c in requirements)
-            {
-                if (Char.IsDigit(c))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+       private static bool IsValid(string[]? args)
+       {
+           var number = int.Parse(args[0]);
+           var requirements = args[1];
+           if (number < 6 || args.Length<2) return false;
+           foreach (var c in requirements)
+           {
+               if (Char.IsDigit(c))
+               {
+                   return false;
+               }
+           }
+           return true;
+       }
 
 
-        static void PasswordGenerator()
-        {
-            ShowHelpText();
-        }
+       static void PasswordGenerator()
+       {
+           ShowHelpText();
+       }
 
-        static void ShowHelpText()
-        {
-            Console.WriteLine("PasswordGenerator  " +
-                              "\nOptions:\n- l = liten bokstav\n- " +
-                              "L = stor bokstav\n- " +
-                              "d = siffer\n- " +
-                              "s = spesialtegn (!\"#\u00a4%&/(){}[]\n-" +
-                              "lengde skal være min 6 tegn\n" +
-                              "Eksempel: PasswordGenerator 14 lLssdd\n" +
-                              "    betyr\n " +
-                              "    Min. 1 liten bokstav\n    " +
-                              "    Min. 1 1 stor bokstav\n   " +
-                              "    Min. 2 spesialtegn\n     " +
-                              "    Min. 2 sifre\n   " +
-                              "    Lengde på passordet skal være 14 tegn ");
-        }
+       static void ShowHelpText()
+       {
+           Console.WriteLine("PasswordGenerator  " +
+                             "\nOptions:\n- l = liten bokstav\n- " +
+                             "L = stor bokstav\n- " +
+                             "d = siffer\n- " +
+                             "s = spesialtegn (!\"#\u00a4%&/(){}[]\n-" +
+                             "lengde skal være min 6 tegn\n" +
+                             "Eksempel: PasswordGenerator 14 lLssdd\n" +
+                             "    betyr\n " +
+                             "    Min. 1 liten bokstav\n    " +
+                             "    Min. 1 1 stor bokstav\n   " +
+                             "    Min. 2 spesialtegn\n     " +
+                             "    Min. 2 sifre\n   " +
+                             "    Lengde på passordet skal være 14 tegn ");
+       }
 
     }
 }
