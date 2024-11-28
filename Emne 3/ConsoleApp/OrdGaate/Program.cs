dc75be6138eb464943efaee68039a8e6a99d60c7
ImPhame <jakobsenkenneth@hotmail.no>
2024-11-28 13:53:12 +0100

    Update

--- Emne 3/ConsoleApp/OrdGaate/Program.cs
+++ Emne 3/ConsoleApp/OrdGaate/Program.cs
@@ -1,26 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Diagnostics.CodeAnalysis;
+﻿namespace OrdGaate;
 
 public static class Program
 {
     private static void Main(string[] args)
-    {
-        string filePath = "ordliste.txt";
-        if(!File.Exists(filePath))
-        {
-            Console.WriteLine("File not found");
-        }
-        else
-        {
-            var textData = File.ReadAllText(filePath);
-            string[]splitData = textData.Split('\t');
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write(splitData[i]);
-            }
-        } 
+    { 
         
+        string filePath = "ordliste.txt";
+       string[] words = GetWords(filePath);
+       FindMatch(words);
+    }
+
+    static string[] GetWords(string args)
+    {
+        var textData = File.ReadAllLines(args);
+        var words = new List<string>();
+        foreach(var line in textData)
+        {
+            var word = line.Split('\t')[1];
+            if (!words.Contains(word) 
+                && word.Length is > 6 and < 10
+                && !word.Contains('-'))
+            {
+                var lastThreeLetters = word.Length - 3;
+                Console.WriteLine($"Word: {word}, lastLetters:{word.Substring(lastThreeLetters)}");
+                words.Add(word);
+            }
+        }
+        return words.ToArray();
+    }
+
+    static string[] FindMatch(string[] words)
+    {
+        return [""];
     }
 
 }
