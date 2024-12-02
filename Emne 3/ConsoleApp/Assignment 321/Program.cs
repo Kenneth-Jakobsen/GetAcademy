4ed04d1ba0b4a16417ca404c25d43843f4e5862b
ImPhame <jakobsenkenneth@hotmail.no>
2024-12-02 09:05:36 +0100

    update

---
+++ Emne 3/ConsoleApp/Assignment 321/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment_321;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a text");
+        string? text = Console.ReadLine();
+        Counter.ShowCounts(text);
+        
+    }
+}
