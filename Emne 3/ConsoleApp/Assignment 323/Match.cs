583db3afe015a17a013a6ff5462ac86233475ca6
ImPhame <jakobsenkenneth@hotmail.no>
2024-12-05 15:16:23 +0100

    Update

---
+++ Emne 3/ConsoleApp/Assignment 323/Match.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_323;
+
+public class Match(string myBet)
+{
+    private  int _homeGoals;
+    private  int _awayGoals;
+    public  bool IsMatchRunning = true;
+    private readonly string? _bet = myBet;
+
+
+    public void Goal(string input)
+    {
+        if (input == "H") _homeGoals++;
+        else _awayGoals++;
+    }
+    
+
+    public void GetResult()
+    {
+        Console.WriteLine(IsMatchRunning
+            ? $"The result is {_homeGoals} for Home and {_awayGoals} for Away."
+            : $"The end result is {_homeGoals} for Home and {_awayGoals} for Away, your bet was {_bet}.");
+    }
+
+    public void EndMatch()
+    {
+        IsMatchRunning = false;
+        GetResult();
+    }
+    
+}
--- Emne 3/ConsoleApp/Assignment 323/Program.cs
+++ Emne 3/ConsoleApp/Assignment 323/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Gyldig tips: \n" +
+                      " - H, U, B\n" + 
+                      " - halvgardering: HU, HB, UB\n" + 
+                      " - helgardering: HUB\n" + 
+                      "Hva har du tippet for denne kampen? ");
+        string bet = Console.ReadLine();
+        var myMatch = new Match(bet);
+        while (myMatch.IsMatchRunning)
+        {
+            Console.Write("Kommandoer: \n" + 
+                          " - H = scoring hjemmelag\n" + 
+                          " - B = scoring bortelag\n" + 
+                          " - X = kampen er ferdig\n" + 
+                          "Angi kommando: ");
+            var command = Console.ReadLine();
+            if (command == "X")
+            {
+                myMatch.EndMatch();
+                break;
+            }
+            if(command == "H" || command == "B")myMatch.Goal(command);
+            myMatch.GetResult();
+        }
+        
     }
 }
