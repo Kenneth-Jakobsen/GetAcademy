ee6acbf908de8211457ee4efe66049984a8e0d67
ImPhame <jakobsenkenneth@hotmail.no>
2024-12-12 09:39:51 +0100

    update

--- Emne 3/ConsoleApp/Assignment 323/Match.cs
+++ Emne 3/ConsoleApp/Assignment 323/Match.cs
@@ -5,7 +5,6 @@ public class Match(string? myBet)
     private  int _homeGoals;
     private  int _awayGoals;
     public  bool IsMatchRunning = true;
-    private readonly string? _bet = myBet;
 
 
     public void Goal(string input)
@@ -14,12 +13,20 @@ public class Match(string? myBet)
         else _awayGoals++;
     }
     
+    private bool IsBetCorrect()
+    {
+        var result = _homeGoals == _awayGoals ? "U" : _homeGoals > _awayGoals ? "H" : "B";
+        return result == myBet;
+    }
+    
 
     public void GetResult()
     {
+        var _result=_homeGoals == _awayGoals ? "U" : _homeGoals > _awayGoals ? "H" : "B";
+        var betResult = IsBetCorrect() ? "Wins": "No Wins";
         Console.WriteLine(IsMatchRunning
             ? $"The result is {_homeGoals} for Home and {_awayGoals} for Away."
-            : $"The end result is {_homeGoals} for Home and {_awayGoals} for Away, your bet was {_bet}.");
+            : $"The end result is {_homeGoals} for Home and {_awayGoals} for Away, Your bet : {betResult} ");
     }
 
     public void EndMatch()
---
+++ Emne 3/ConsoleApp/CodeAlong/textAnalyze.cs
@@ -0,0 +1,66 @@
+﻿namespace CodeAlong;
+
+public class textAnalyze
+{
+    private string text;
+    private string[] words;
+    private string[] letters;
+    private string longestWord="";
+    private int letterCount;
+
+    public textAnalyze(string text)
+    {
+        this.text = text;
+    }
+    
+
+    public string analyzeText()
+    {
+        int wordCount = 0;
+        string[] words = text.Split(" ");
+        foreach (var word in words)
+        {
+            findLongestWord(word);
+            countLetters(word);
+            wordCount++;
+        }
+        return ($"Denne teksten har {wordCount} ord, det lengste ordet er {longestWord}. Teksten har {letterCount} ord");
+    }
+
+    private string findLongestWord(string word)
+    {
+        if (word.Length > longestWord.Length)
+        {
+            var cleantext = cleanText(word);
+            if (cleantext.Length > longestWord.Length)
+            {
+                longestWord = cleantext;
+            }
+        }
+        return longestWord;
+    }
+
+    private string cleanText(string text)
+    {
+        var cleanedText = "";
+        foreach (char c in text)
+        {
+            if (char.IsLetter(c))
+            {
+                cleanedText += c;
+            }
+        }
+        return cleanedText;
+    }
+    
+    private void countLetters(string word)
+    { 
+        foreach (char c in word)
+        {
+            if (char.IsLetter(c))
+            {
+                letterCount ++;
+            }
+        }
+    }
+}
