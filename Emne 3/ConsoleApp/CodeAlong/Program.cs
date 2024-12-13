208aaf45d5851c76c480ca7d61a92fc34d37e3b3
ImPhame <jakobsenkenneth@hotmail.no>
2024-12-13 13:17:16 +0100

    update

---
+++ Emne 3/ConsoleApp/CodeAlong/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeAlong;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var program = new textAnalyze("Den lille katten sov søtt på den gamle, slitte sofaen!!.");
+        program.analyzeText();
+        //program.cleanText("Sofa!!..");
+        // tar imot string
+        // string > array
+        // loope over textarray
+        // telle bokstaver
+    }
+}
