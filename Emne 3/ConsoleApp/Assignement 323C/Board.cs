dd1c01b08bf972798e2823ca1732246c94bd314b
ImPhame <jakobsenkenneth@hotmail.no>
2025-01-09 10:46:37 +0100

    x

---
+++ Emne 3/ConsoleApp/Assignement 323C/Board.cs
@@ -0,0 +1,6 @@
+﻿namespace Assignement_323C;
+
+public class Board
+{
+    Square[] _squares = new Square[9];
+}
--- Emne 3/ConsoleApp/Assignement 323C/Square.cs
+++ Emne 3/ConsoleApp/Assignement 323C/Square.cs
@@ -2,5 +2,25 @@
 
 public class Square
 {
+    private bool _isEmpty = true;
+    private string _state = string.Empty;
+
+    public bool IsEmpty()
+    {
+        return _isEmpty;
+    }
+
+    public string State()
+    {
+        return _state;
+    }
+
+    public void SetState(bool state)
+    {
+        if (!_isEmpty)
+        {
+            _state = state ? "player 1" : "player 2";
+        }
+    }
     
 }
