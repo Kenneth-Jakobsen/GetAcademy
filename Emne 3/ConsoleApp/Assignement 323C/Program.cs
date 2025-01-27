76fe5c927fc7598e8ad4f230bb4a5f6c4e27d2c7
ImPhame <jakobsenkenneth@hotmail.no>
2025-01-27 10:47:10 +0100

    x

---
+++ Emne 3/ConsoleApp/Assignement 323C/Program.cs
@@ -0,0 +1,12 @@
+﻿using Assignement_323C;
+
+var board = new Board();
+var gameConsole = new GameConsole(board);
+while (true)
+{
+    gameConsole.Show(board);
+    
+    Console.Write("Skriv inn hvor du vil sette kryss (f.eks. \"a2\"): ");
+    var position = Console.ReadLine(); 
+    if (position != null) board.Mark(position);
+}
